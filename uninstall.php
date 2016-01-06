aa9a1e688c7ce2b814da4ec7bc1d68f7ddff10b8
kcppdevelopers <kcppdevelopers@gmail.com>
2016-01-06 18:32:27 +0530

    Remove any unnecessary table creation, ref #37

--- uninstall.php
+++ uninstall.php
@@ -6,6 +6,3 @@
 if (!defined('WP_UNINSTALL_PLUGIN')) {
     exit;
 }
-global $wpdb;
-$paypal_security_tbl = $wpdb->prefix . 'paypal_security_reports';
-$wpdb->query("DROP TABLE IF EXISTS $paypal_security_tbl");
